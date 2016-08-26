1e479b56a701aaec30097bb247a01f2c9187359f
Jerry Lee <jerry.lee09@yahoo.com>
2016-08-26 07:48:55 -0500

    add delete button

--- formOnSteroid.js
+++ formOnSteroid.js
@@ -18,33 +18,13 @@ myApp.controller('employeeController', ['$scope', function($scope) {
 
 		$scope.total += ($scope.employee.salary / 12);
 	}
-	$scope.DeleteInfo = function () {
-		
-		$scope.total -= ($scope.employee.salary/12);
-		this.parentNode.splice(0);
+	$scope.DeleteInfo = function(employee) { 
+	  var index = $scope.employees.indexOf(employee);
+	  console.log(index);
+	  $scope.employees.splice(index, 1);
+	  $scope.total -= ($scope.employee.salary / 12);
+
 	}
-	
-  //   $scope.increment = function(number) {
-  //   $scope.total += number;
-  // }
 
-
-  //   $('#container').on('click','button', function() {
-  //     var personPrice = $(this).data("salary");
-  //     var totalPrice = $(totalSal());
-  //     var newTotal = totalPrice[0] - personPrice;
-
-  //     $('.totalSalary').append(newTotal);
-  //     $(this).parent().remove();
-      
-  //     // console.log(totalPrice[0]);
-  //     // console.log($(this).data("salary"));
-  //     // console.log(newTotal);
-  //     // console.log(typeof totalPrice);
-  //     // console.log('total price',totalPrice);
-  //     // console.log('total price', totalPrice[0]);
-  //     // console.log(typeof personPrice);
-  //     totalSal();
-  //   });
 
 }]);
